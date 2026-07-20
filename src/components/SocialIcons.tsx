86a215f4d193f8948f4c2aca616cbce428d9d5b5
Nishant232 <nishantstar400@gmail.com>
2026-07-20 14:41:17 +0530

    first commit

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span> */}
       </div>
-      <a className="resume-button" href="https://drive.google.com/file/d/1N0XD_cvrkVGMuldMjF7xh4nX-K8PHUy-/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+      <a className="resume-button" href="https://drive.google.com/file/d/1b7XYnLrGDk26cGdMhHuZ9LZ8BnhttgaL/view?usp=sharing" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
